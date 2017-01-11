e561c14e25a55923da3ad6455df0a8f09b9b7bce
你的名字 <email@xxx.xxx>
2017-01-11 09:07:59 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :jobs do
-    resources :resumes
-  end
-  root 'jobs#index'
-
   namespace :admin do
     resources :jobs do
       member do
         post :publish
         post :hide
       end
+
+      resources :resumes
     end
   end
+
+  root 'jobs#index'
 end
